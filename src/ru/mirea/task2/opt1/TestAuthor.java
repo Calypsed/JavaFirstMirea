f4295af140213121ee0f23f768682a3fd5cab961
Yakovlev Kirill <kyakovlev27@vk.com>
2025-09-29 13:58:07 +0300

    Task 2 Opt 1 Done

---
+++ src/ru/mirea/task2/opt1/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.task2.opt1;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author Sand = new Author("Brandon Sanderson", "stormlight@archive.com", 'M');
+        System.out.print(Sand.toString());
+    }
+}
